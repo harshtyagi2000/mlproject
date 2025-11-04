909f9ea71a7a1dadefee0c62348b1d6becad0882
harshtyagi2000 <harshtyagi2111@gmail.com>
2025-11-04 21:40:26 +0530

    Data Transformation

--- src/exception.py
+++ src/exception.py
@@ -22,7 +22,8 @@ class CustomException(Exception):
 # if __name__ == "__main__":
 
 #     try:
-#         a = 1 / 0
+#     
+#         # a = 1 / 0
 #     except Exception as e:
 #         logging.info("Divide by Zero")
 #         raise CustomException(e, sys)    
